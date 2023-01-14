5f187ff77b5e1c2b279f1485ca8e80d918d3176b
MargoOgram <109178025+MargoOgram@users.noreply.github.com>
2023-01-15 00:23:47 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,21 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
+
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
-if (localStorage.getItem('videoplayer-current-time')) {
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time')); 
-} 
+
+
+
 
 function onPlay({ seconds }) {
+
   localStorage.setItem('videoplayer-current-time', seconds);
+
+  if (localStorage.getItem('videoplayer-current-time')) {
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')); 
+} 
 }
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
