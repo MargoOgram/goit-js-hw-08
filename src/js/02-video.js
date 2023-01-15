ac85d52f7e88a1afe156b24f2d90c89d3dad801c
MargoOgram <109178025+MargoOgram@users.noreply.github.com>
2023-01-15 14:22:45 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,16 @@
 import throttle from 'lodash.throttle';
-import Player from '@vimeo/player'; 
-
+import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
 player.on('timeupdate', throttle(onPlay, 1000));
 if (localStorage.getItem('videoplayer-current-time')) {
   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 }
-function onPlay({ seconds }) {
+function onPlay({ seconds, duration }) {
   localStorage.setItem('videoplayer-current-time', seconds);
+  if (seconds === duration) {
+    localStorage.removeItem('videoplayer-current-time');
+  }
 }
 
 
-
-
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-// import throttle from 'lodash.throttle';
-// import player from '@vimeo/player';
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-// player.on('timeupdate', throttle(onPlay, 1000));
-// function onPlay({ seconds }) {
-//     localStorage.setItem('videoplayer-current-time', seconds);
-// }
-//           if (localStorage.getItem('videoplayer-current-time')) {
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time')); 
-// }  
-
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
