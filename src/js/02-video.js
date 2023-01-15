f58690e2328679ba03774a101cb8d82f2e25c048
MargoOgram <109178025+MargoOgram@users.noreply.github.com>
2023-01-15 14:04:44 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,9 @@
 import throttle from 'lodash.throttle';
-import Player from '@vimeo/player';
+import Player from '@vimeo/player'; 
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
 player.on('timeupdate', throttle(onPlay, 1000));
 if (localStorage.getItem('videoplayer-current-time')) {
   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
@@ -9,26 +11,18 @@ if (localStorage.getItem('videoplayer-current-time')) {
 function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
+
+
+
+
 // player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
 // import throttle from 'lodash.throttle';
 // import player from '@vimeo/player';
-
 // const iframe = document.querySelector('iframe');
 // const player = new Vimeo.Player(iframe);
-
-
-
-
-
-
 // player.on('timeupdate', throttle(onPlay, 1000));
-
-
 // function onPlay({ seconds }) {
 //     localStorage.setItem('videoplayer-current-time', seconds);
-
 // }
 //           if (localStorage.getItem('videoplayer-current-time')) {
 // player.setCurrentTime(localStorage.getItem('videoplayer-current-time')); 
