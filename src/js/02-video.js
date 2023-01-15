13e9be952f87ef937d7eada41e64cc1095d0af7c
MargoOgram <109178025+MargoOgram@users.noreply.github.com>
2023-01-15 13:48:56 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,37 @@
 import throttle from 'lodash.throttle';
-
+import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-
-
+const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
-
-
-
-
-
-function onPlay({ seconds }) {
-localStorage.setItem('videoplayer-current-time', seconds);
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 }
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
-  if (localStorage.getItem('videoplayer-current-time')) {
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time')); 
-}  
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// import throttle from 'lodash.throttle';
+// import player from '@vimeo/player';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+
+
+
+
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+
+// function onPlay({ seconds }) {
+//     localStorage.setItem('videoplayer-current-time', seconds);
+
+// }
+//           if (localStorage.getItem('videoplayer-current-time')) {
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time')); 
+// }  
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
